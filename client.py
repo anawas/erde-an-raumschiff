a890855740a6fa58ac91f46d56d35d2f31c882dc
Andreas Wassmer <andreas.wassmer@fhnw.ch>
2024-11-29 08:22:11 +0100

    Code cleanup

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
-import os
-from webdav3.client import Client
-from dotenv import load_dotenv
 import logging
+import os
+
+from dotenv import load_dotenv
+from webdav3.client import Client, WebDavException
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("logclient")
@@ -16,18 +17,29 @@ options = {
     "verbose": True,
 }
 
-logger.info(f"Connecting to server {os.getenv("HOST_URL")}")
-try:
-    client = Client(options)
-    logger.info("Success")
-except Exception as e:
-    logger.error(f"Could not connect, cause {e.args}")
-    exit(-1)
+def get_files(client: Client, remote_dir: str, local_dir:str):
+    client.download_directory(remote_dir, local_dir)
 
-files = client.list("/eCallisto/bursts", get_info=True)
-for i in range(1, len(files)):
-    # First entry is current dir
-    file_info = files[i]
-    if file_info["isdir"]:
-        print(f"{file_info["path"]}")
 
+def main():
+    logger.info(f"Connecting to server {os.getenv("HOST_URL")}")
+    try:
+        client = Client(options)
+        logger.info("Success")
+    except Exception as e:
+        logger.error(f"Could not connect, cause {e.args}")
+        exit(-1)
+
+    files = client.list("/eCallisto/bursts", get_info=True)
+    for i in range(1, len(files)):
+        # First entry is current dir
+        file_info = files[i]
+        if file_info["isdir"]:
+            dir_name = file_info["path"].rstrip()
+            print(f"{os.path.dirname(dir_name)}")
+
+    logger.info("Downloading data")
+    get_files(client, "/eCallisto/bursts/type_I", "e:\\Daten\\ecallisto")
+
+if __name__ == "__main__":
+    main()
