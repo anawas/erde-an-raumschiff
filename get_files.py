d6e7558594e41800b39904da6cb147cee958af78
anawas <andreas.wassmer@me.com>
2025-08-10 10:56:05 +0200

    get files in folder

--- get_files.py
+++ get_files.py
@@ -19,6 +19,10 @@ options = {
     "verbose": True,
 }
 
+def get_file_list(client: Client, remote_dir: str) -> list[str]:
+    files = client.list(remote_dir, get_info=True)
+    return files
+
 def get_files(client: Client, remote_dir: str, local_dir:str) -> None:
     """Downloads all the files in `remote_dir`
 
@@ -27,11 +31,11 @@ def get_files(client: Client, remote_dir: str, local_dir:str) -> None:
         remote_dir (str): The folder path on remote server
         local_dir (str): Where to download the files
     """
-    def callback(current, total):
-        if current == 0:
-            print(f"Downloading {total} bytes")
-        
-    client.download_directory(remote_dir, local_dir, progress=callback)
+
+    logger.info(f"Getting files in folder {path}")
+    files = get_file_list(client, path)
+    logger.info(f"Found {len(files)} files")
+    client.download_sync(remote_dir, local_dir)
 
 def get_folders(client: Client, remote_base_dir: str) -> List[str]:
     """Gets a list of path names where burst image can be found¨
@@ -77,7 +81,10 @@ def main():
     # for i in trange(0, len(path_names)):
     path = path_names[2]
     last_folder = path.split("/")[-2]
-    get_files(client, path, f"./data/{last_folder}")
+    files = get_file_list(client, path)
+    for i in range(5):
+        print(f"{path}/{files[i]}")
+    #get_files(client, path, f"./data/{last_folder}")
 
 if __name__ == "__main__":
     main()
